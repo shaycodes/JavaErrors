a8f696a638737d6068325cc18d5e384d123e0ad9
Shay Lindsey <shaycodes@gmail.com>
2022-10-14 23:29:44 -0700

    Add files via upload
    Java Types, Variables, & Branching

---
+++ src/Errors.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Errors {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        String ourNum = 42;
+        double time = "Two Hours";
+        int x = 2;
+        int y;
+        char blue = true;
+        System.out.printf(x + y);
+        System.out.println(blue);
+        System.out.println(time);
+        System.out.printf(ourNum);
+    }
+    }
+}
